b2e99448e28eb0119244e10ab1fa61e881dfdbdf
Isaak Haas <v-ialdape@expediagroup.com>
2022-10-22 00:02:08 -0500

    configurations of project

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,26 +9,27 @@ plugins {
 
 group = "com.expenses.app.server"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-    implementation("org.springframework.boot:spring-boot-starter-security")
+    //implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    //implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("com.amazonaws:aws-java-sdk:1.12.322")
+    //implementation("com.amazonaws:aws-java-sdk:1.12.322")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
     // SQL ORM
-    runtimeOnly("org.postgresql:postgresql")
+    implementation("org.postgresql:postgresql:42.5.0")
     implementation("org.jetbrains.exposed:exposed-core:0.40.1")
     implementation("org.jetbrains.exposed:exposed-dao:0.40.1")
     implementation("org.jetbrains.exposed:exposed-jdbc:0.40.1")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
 }
@@ -36,7 +37,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
+        jvmTarget = "11"
     }
 }
 
