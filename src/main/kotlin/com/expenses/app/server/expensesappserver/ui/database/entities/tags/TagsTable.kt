02808e5bf560e85594f5f645e723d1daf9c9acc8
isaakavo <isaakhaas96@gmail.com>
2023-02-17 23:57:25 -0600

    feat: change of id from int to uuid

--- src/main/kotlin/com/expenses/app/server/expensesappserver/ui/database/entities/tags/TagsTable.kt
+++ src/main/kotlin/com/expenses/app/server/expensesappserver/ui/database/entities/tags/TagsTable.kt
@@ -1,28 +1,29 @@
 package com.expenses.app.server.expensesappserver.ui.database.entities.tags
 
-import org.jetbrains.exposed.dao.IntEntity
-import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.UUIDEntity
+import org.jetbrains.exposed.dao.UUIDEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
-import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.dao.id.UUIDTable
 import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.javatime.datetime
 import java.time.LocalDateTime
+import java.util.UUID
 
 data class Tags(
-    val id: Int? = null,
+    val id: UUID? = null,
     val tagName: String,
     val dateAdded: LocalDateTime
 )
 
-object TagsTable: IntIdTable("tag") {
+object TagsTable: UUIDTable("tag", "uuid") {
     val tagName: Column<String> = varchar("tag_name", 25).uniqueIndex()
     val dateAdded: Column<LocalDateTime> = datetime("date_added")
 }
 
 class TagEntity(
-    id: EntityID<Int>
-): IntEntity(id) {
-    companion object: IntEntityClass<TagEntity>(TagsTable)
+    id: EntityID<UUID>
+): UUIDEntity(id) {
+    companion object: UUIDEntityClass<TagEntity>(TagsTable)
 
     var tagName by TagsTable.tagName
     var dateAdded by TagsTable.dateAdded
